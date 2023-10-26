e8ed9e212134e98b4a2e8d1f3695f1cc0618a47c
xikangfang <fangxikang@bytedance.com>
2023-10-26 19:26:31 +0800

    feat(*): support set mainhost

--- host_availabler_factory.go
+++ host_availabler_factory.go
@@ -1,12 +1,12 @@
 package core
 
 type HostAvailablerFactory interface {
-	NewHostAvailabler(projectID string, hosts []string) (HostAvailabler, error)
+	NewHostAvailabler(projectID string, hosts []string, mainHost string, skipFetchHosts bool) (HostAvailabler, error)
 }
 
 type HostAvailablerFactoryBase struct {
 }
 
-func (h *HostAvailablerFactoryBase) NewHostAvailabler(projectID string, hosts []string) (HostAvailabler, error) {
-	return NewPingHostAvailabler(hosts, projectID, nil)
+func (h *HostAvailablerFactoryBase) NewHostAvailabler(projectID string, hosts []string, mainHost string, skipFetchHosts bool) (HostAvailabler, error) {
+	return NewPingHostAvailabler(hosts, projectID, nil, mainHost, skipFetchHosts)
 }
